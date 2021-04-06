09c9811e3272845a56d182fe08251dbd2fb076f8
Andres <andresjml@gmail.com>
2021-04-06 12:52:44 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-
+function superbowlWin(arr) {
+    let obj = arr.find( result => result.result === "W" )
+    if(!!obj){
+        return obj.year
+    }
+    else{
+        return undefined;
+    }
+    
+  }
